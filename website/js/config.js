9fc05ee187322b46efbd34ec88be8af2508835dd
unknown <ziang6@ualberta.ca>
2024-06-07 18:34:53 -0600

    new config

--- website/js/config.js
+++ website/js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: 'ap-south-1_8f6D0LI5g', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'crgrg1v40odp5tq2v4jbia2lt', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: 'ap-south-1' // e.g. us-east-2
+        userPoolId: 'us-east-2_UdjXQWcPZ', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2ipl27m4e5rac01sece1m9b76i', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod,
